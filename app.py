0cadbc26407f75d3ec54c7022f11305b9cef5bb9
Muhammad Usman <m.usman@it22.nl>
2025-11-14 16:34:09 +0500

    fix proc

--- app.py
+++ app.py
@@ -112,5 +112,8 @@ def upload():
     output_path = process_docx(file_path)
     return send_file(output_path, as_attachment=True)
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
